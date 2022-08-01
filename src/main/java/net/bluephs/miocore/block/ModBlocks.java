a0c4da455bcf6a5071ddba43bd1e8045ccbd6874
Blue.PHS <bluelp@gmx.de>
2022-08-01 11:57:36 +0200

    Reworked some Codesnippets

--- src/main/java/net/bluephs/miocore/block/ModBlocks.java
+++ src/main/java/net/bluephs/miocore/block/ModBlocks.java
@@ -25,56 +25,69 @@ import java.util.List;
 
 public class ModBlocks {
 
-    public static final Block NICKEL_BLOCK = registerBlock("nickel_block",
-            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.Mio);
+    //region Normal Ores
+
     public static final Block NICKEL_ORE = registerBlock("nickel_ore",
             new Block(FabricBlockSettings.of(Material.STONE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
-    public static final Block DEEPSLATE_NICKEL_ORE = registerBlock("deepslate_nickel_ore",
-            new Block(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_COPPER_ORE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
-
-    public static final Block LEAD_BLOCK = registerBlock("lead_block",
-            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.Mio);
     public static final Block LEAD_ORE = registerBlock("lead_ore",
             new Block(FabricBlockSettings.of(Material.STONE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
-    public static final Block DEEPSLATE_LEAD_ORE = registerBlock("deepslate_lead_ore",
-            new Block(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_COPPER_ORE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
-
-    public static final Block ALUMINIUM_BLOCK = registerBlock("aluminium_block",
-            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.Mio);
     public static final Block ALUMINIUM_ORE = registerBlock("aluminium_ore",
             new Block(FabricBlockSettings.of(Material.STONE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
-    public static final Block DEEPSLATE_ALUMINIUM_ORE = registerBlock("deepslate_aluminium_ore",
-            new Block(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_COPPER_ORE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
-
-    public static final Block PLATINUM_BLOCK = registerBlock("platinum_block",
-            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.Mio);
     public static final Block PLATINUM_ORE = registerBlock("platinum_ore",
             new Block(FabricBlockSettings.of(Material.STONE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
-    public static final Block DEEPSLATE_PLATINUM_ORE = registerBlock("deepslate_platinum_ore",
-            new Block(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_COPPER_ORE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
-
-    public static final Block SILVER_BLOCK = registerBlock("silver_block",
-            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.Mio);
     public static final Block SILVER_ORE = registerBlock("silver_ore",
             new Block(FabricBlockSettings.of(Material.STONE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
-    public static final Block DEEPSLATE_SILVER_ORE = registerBlock("deepslate_silver_ore",
-            new Block(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_COPPER_ORE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
-
-    public static final Block ARDITE_BLOCK = registerBlock("ardite_block",
-            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.Mio);
-    public static final Block NETHERRACK_ARDITE_ORE = registerBlock("netherrack_ardite_ore",
-            new Block(FabricBlockSettings.copyOf(Blocks.NETHER_GOLD_ORE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
-
-    public static final Block OSMIUM_BLOCK = registerBlock("osmium_block",
-            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.Mio);
-    public static final Block NETHERRACK_OSMIUM_ORE = registerBlock("netherrack_osmium_ore",
-            new Block(FabricBlockSettings.copyOf(Blocks.NETHER_GOLD_ORE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
-
     public static final Block URANIUM_ORE = registerBlock("uranium_ore",
             new Block(FabricBlockSettings.of(Material.STONE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
+
+    //endregion
+
+    //region Deepslate Ores
+
+    public static final Block DEEPSLATE_NICKEL_ORE = registerBlock("deepslate_nickel_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_COPPER_ORE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
+    public static final Block DEEPSLATE_LEAD_ORE = registerBlock("deepslate_lead_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_COPPER_ORE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
+    public static final Block DEEPSLATE_ALUMINIUM_ORE = registerBlock("deepslate_aluminium_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_COPPER_ORE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
+    public static final Block DEEPSLATE_PLATINUM_ORE = registerBlock("deepslate_platinum_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_COPPER_ORE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
+    public static final Block DEEPSLATE_SILVER_ORE = registerBlock("deepslate_silver_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_COPPER_ORE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
     public static final Block DEEPSLATE_URANIUM_ORE = registerBlock("deepslate_uranium_ore",
             new Block(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_COPPER_ORE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
 
+    //endregion
+
+    //region Nether Ores
+
+    public static final Block NETHERRACK_ARDITE_ORE = registerBlock("netherrack_ardite_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.NETHER_GOLD_ORE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
+    public static final Block NETHERRACK_OSMIUM_ORE = registerBlock("netherrack_osmium_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.NETHER_GOLD_ORE).strength(4.5f).requiresTool()), ModItemGroup.Mio);
+
+    //endregion
+
+    //region Blocks
+
+    public static final Block NICKEL_BLOCK = registerBlock("nickel_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.Mio);
+    public static final Block LEAD_BLOCK = registerBlock("lead_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.Mio);
+    public static final Block ALUMINIUM_BLOCK = registerBlock("aluminium_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.Mio);
+    public static final Block PLATINUM_BLOCK = registerBlock("platinum_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.Mio);
+    public static final Block SILVER_BLOCK = registerBlock("silver_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.Mio);
+    public static final Block ARDITE_BLOCK = registerBlock("ardite_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.Mio);
+    public static final Block OSMIUM_BLOCK = registerBlock("osmium_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.Mio);
+
+    //endregion
+
+    //region Mahagony
 
     public static final Block MAHAGONY_BUTTON = registerBlock("mahagony_button",
             new ModWoodButtonBlock(FabricBlockSettings.of(Material.WOOD).strength(4.5f).requiresTool().noCollision()), ModItemGroup.Mio);
@@ -87,7 +100,8 @@ public class ModBlocks {
     public static final Block MAHAGONY_PLANKS = registerBlock("mahagony_planks",
             new Block(FabricBlockSettings.copy(Blocks.OAK_PLANKS).requiresTool()), ModItemGroup.Mio);
     public static final Block MAHAGONY_STAIRS = registerBlock("mahagony_stairs",
-            new ModStairsBlock(ModBlocks.MAHAGONY_PLANKS.getDefaultState(), FabricBlockSettings.of(Material.WOOD).strength(4.5f).requiresTool()), ModItemGroup.Mio);
+            new ModStairsBlock(ModBlocks.MAHAGONY_PLANKS.getDefaultState(),
+                    FabricBlockSettings.of(Material.WOOD).strength(4.5f).requiresTool()), ModItemGroup.Mio);
     public static final Block MAHAGONY_LOG = registerBlock("mahagony_log",
             new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_LOG).strength(4.5f).requiresTool()), ModItemGroup.Mio);
     public static final Block MAHAGONY_WOOD = registerBlock("mahagony_wood",
@@ -101,6 +115,9 @@ public class ModBlocks {
     public static final Block MAHAGONY_SAPLING = registerBlock("mahagony_sapling",
             new ModSaplingBlock(new MahagonySaplingGenerator(),FabricBlockSettings.copy(Blocks.OAK_SAPLING)), ModItemGroup.Mio);
 
+    //endregion
+
+    //region Ash Tree
 
     public static final Block ASH_TREE_BUTTON = registerBlock("ash_tree_button",
             new ModWoodButtonBlock(FabricBlockSettings.of(Material.WOOD).strength(4.5f).requiresTool().noCollision()), ModItemGroup.Mio);
@@ -113,7 +130,8 @@ public class ModBlocks {
     public static final Block ASH_TREE_PLANKS = registerBlock("ash_tree_planks",
             new Block(FabricBlockSettings.copy(Blocks.OAK_PLANKS).requiresTool()), ModItemGroup.Mio);
     public static final Block ASH_TREE_STAIRS = registerBlock("ash_tree_stairs",
-            new ModStairsBlock(ModBlocks.ASH_TREE_PLANKS.getDefaultState(), FabricBlockSettings.of(Material.WOOD).strength(4.5f).requiresTool()), ModItemGroup.Mio);
+            new ModStairsBlock(ModBlocks.ASH_TREE_PLANKS.getDefaultState(),
+                    FabricBlockSettings.of(Material.WOOD).strength(4.5f).requiresTool()), ModItemGroup.Mio);
     public static final Block ASH_TREE_LOG = registerBlock("ash_tree_log",
             new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_LOG).strength(4.5f).requiresTool()), ModItemGroup.Mio);
     public static final Block ASH_TREE_WOOD = registerBlock("ash_tree_wood",
@@ -127,10 +145,10 @@ public class ModBlocks {
     public static final Block ASH_TREE_SAPLING = registerBlock("ash_tree_sapling",
             new ModSaplingBlock(new AshTreeSaplingGenerator(),FabricBlockSettings.copy(Blocks.OAK_SAPLING)), ModItemGroup.Mio);
 
-    public static final Block CHERRY_TREE_LOG = registerBlock("cherry_tree_log",
-            new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_LOG).strength(4.5f).requiresTool()), ModItemGroup.Mio);
-    public static final Block CHERRY_TREE_WOOD = registerBlock("cherry_tree_wood",
-            new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_WOOD).strength(4.5f).requiresTool()), ModItemGroup.Mio);
+    //endregion
+
+    //region Cherry Tree
+
     public static final Block CHERRY_TREE_BUTTON = registerBlock("cherry_tree_button",
             new ModWoodButtonBlock(FabricBlockSettings.of(Material.WOOD).strength(4.5f).requiresTool().noCollision()), ModItemGroup.Mio);
     public static final Block CHERRY_TREE_FENCE = registerBlock("cherry_tree_fence",
@@ -143,8 +161,12 @@ public class ModBlocks {
             new Block(FabricBlockSettings.copy(Blocks.OAK_PLANKS).requiresTool()), ModItemGroup.Mio);
 
     public static final Block CHERRY_TREE_STAIRS = registerBlock("cherry_tree_stairs",
-            new ModStairsBlock(ModBlocks.CHERRY_TREE_PLANKS.getDefaultState(), FabricBlockSettings.of(Material.WOOD).strength(4.5f).requiresTool()), ModItemGroup.Mio);
-
+            new ModStairsBlock(ModBlocks.CHERRY_TREE_PLANKS.getDefaultState(),
+                    FabricBlockSettings.of(Material.WOOD).strength(4.5f).requiresTool()), ModItemGroup.Mio);
+    public static final Block CHERRY_TREE_LOG = registerBlock("cherry_tree_log",
+            new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_LOG).strength(4.5f).requiresTool()), ModItemGroup.Mio);
+    public static final Block CHERRY_TREE_WOOD = registerBlock("cherry_tree_wood",
+            new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_WOOD).strength(4.5f).requiresTool()), ModItemGroup.Mio);
     public static final Block CHERRY_TREE_DOOR = registerBlock("cherry_tree_door",
             new ModDoorBlock(FabricBlockSettings.of(Material.WOOD).strength(4.5f).requiresTool().nonOpaque()), ModItemGroup.Mio);
     public static final Block CHERRY_TREE_TRAPDOOR = registerBlock("cherry_tree_trapdoor",
@@ -154,9 +176,14 @@ public class ModBlocks {
     public static final Block CHERRY_TREE_SAPLING = registerBlock("cherry_tree_sapling",
             new ModSaplingBlock(new CherryTreeSaplingGenerator(),FabricBlockSettings.copy(Blocks.OAK_SAPLING)), ModItemGroup.Mio);
 
+    //endregion
+
+    //region Special Blocks
+
     public static final Block MIOCORE_BLASTER = registerBlock("miocore_blaster",
             new MiocoreBlasterBlock(FabricBlockSettings.of(Material.METAL).nonOpaque()), ModItemGroup.Mio);
 
+    //endregion
 
 
     private static Block registerBlock(String name, Block block, ItemGroup group, String tooltipKey){
--- src/main/java/net/bluephs/miocore/item/ModItems.java
+++ src/main/java/net/bluephs/miocore/item/ModItems.java
@@ -11,66 +11,78 @@ import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
+    //region Raw Items
+
     public static final Item RAW_NICKEL = registerItem("raw_nickel",
             new Item(new FabricItemSettings().group(ModItemGroup.Mio)));
-    public static final Item NICKEL_INGOT = registerItem("nickel_ingot",
-            new Item(new FabricItemSettings().group(ModItemGroup.Mio)));
-    public static final Item NICKEL_NUGGET = registerItem("nickel_nugget",
-            new Item(new FabricItemSettings().group(ModItemGroup.Mio)));
-
     public static final Item RAW_LEAD = registerItem("raw_lead",
+            new Item(new FabricItemSettings().group(ModItemGroup.Mio)));
+    public static final Item RAW_ALUMINIUM = registerItem("raw_aluminium",
+            new Item(new FabricItemSettings().group(ModItemGroup.Mio)));
+    public static final Item RAW_PLATINUM = registerItem("raw_platinum",
+            new Item(new FabricItemSettings().rarity(Rarity.RARE).group(ModItemGroup.Mio)));
+    public static final Item RAW_SILVER = registerItem("raw_silver",
+            new Item(new FabricItemSettings().group(ModItemGroup.Mio)));
+    public static final Item RAW_ARDITE = registerItem("raw_ardite",
+            new Item(new FabricItemSettings().rarity(Rarity.RARE).group(ModItemGroup.Mio)));
+    public static final Item RAW_OSMIUM = registerItem("raw_osmium",
+            new Item(new FabricItemSettings().rarity(Rarity.RARE).group(ModItemGroup.Mio)));
+    public static final Item RAW_URANIUM = registerItem("raw_uranium",
+            new Item(new FabricItemSettings().rarity(Rarity.EPIC).group(ModItemGroup.Mio)));
+
+    //endregion
+
+    //region Ingots
+
+    public static final Item NICKEL_INGOT = registerItem("nickel_ingot",
             new Item(new FabricItemSettings().group(ModItemGroup.Mio)));
     public static final Item LEAD_INGOT = registerItem("lead_ingot",
             new Item(new FabricItemSettings().group(ModItemGroup.Mio)));
-    public static final Item LEAD_NUGGET = registerItem("lead_nugget",
-            new Item(new FabricItemSettings().group(ModItemGroup.Mio)));
-
-    public static final Item RAW_ALUMINIUM = registerItem("raw_aluminium",
-            new Item(new FabricItemSettings().group(ModItemGroup.Mio)));
     public static final Item ALUMINIUM_INGOT = registerItem("aluminium_ingot",
+            new Item(new FabricItemSettings().group(ModItemGroup.Mio)));
+    public static final Item PLATINUM_INGOT = registerItem("platinum_ingot",
+            new Item(new FabricItemSettings().rarity(Rarity.RARE).group(ModItemGroup.Mio)));
+    public static final Item SILVER_INGOT = registerItem("silver_ingot",
+            new Item(new FabricItemSettings().group(ModItemGroup.Mio)));
+    public static final Item ARDITE_INGOT = registerItem("ardite_ingot",
+            new Item(new FabricItemSettings().rarity(Rarity.RARE).group(ModItemGroup.Mio)));
+    public static final Item OSMIUM_INGOT = registerItem("osmium_ingot",
+            new Item(new FabricItemSettings().rarity(Rarity.RARE).group(ModItemGroup.Mio)));
+    public static final Item URANIUM_INGOT = registerItem("uranium_ingot",
+            new Item(new FabricItemSettings().rarity(Rarity.EPIC).group(ModItemGroup.Mio)));
+
+    //endregion
+
+    //region Nuggets
+
+    public static final Item NICKEL_NUGGET = registerItem("nickel_nugget",
+            new Item(new FabricItemSettings().group(ModItemGroup.Mio)));
+    public static final Item LEAD_NUGGET = registerItem("lead_nugget",
             new Item(new FabricItemSettings().group(ModItemGroup.Mio)));
     public static final Item ALUMINIUM_NUGGET = registerItem("aluminium_nugget",
             new Item(new FabricItemSettings().group(ModItemGroup.Mio)));
-
-    public static final Item RAW_PLATINUM = registerItem("raw_platinum",
-            new Item(new FabricItemSettings().rarity(Rarity.RARE).group(ModItemGroup.Mio)));
-    public static final Item PLATINUM_INGOT = registerItem("platinum_ingot",
-            new Item(new FabricItemSettings().rarity(Rarity.RARE).group(ModItemGroup.Mio)));
     public static final Item PLATINUM_NUGGET = registerItem("platinum_nugget",
             new Item(new FabricItemSettings().rarity(Rarity.RARE).group(ModItemGroup.Mio)));
-
-    public static final Item RAW_SILVER = registerItem("raw_silver",
-            new Item(new FabricItemSettings().group(ModItemGroup.Mio)));
-    public static final Item SILVER_INGOT = registerItem("silver_ingot",
-            new Item(new FabricItemSettings().group(ModItemGroup.Mio)));
     public static final Item SILVER_NUGGET = registerItem("silver_nugget",
             new Item(new FabricItemSettings().group(ModItemGroup.Mio)));
-
-    public static final Item RAW_ARDITE = registerItem("raw_ardite",
-            new Item(new FabricItemSettings().rarity(Rarity.RARE).group(ModItemGroup.Mio)));
-    public static final Item ARDITE_INGOT = registerItem("ardite_ingot",
-            new Item(new FabricItemSettings().rarity(Rarity.RARE).group(ModItemGroup.Mio)));
     public static final Item ARDITE_NUGGET = registerItem("ardite_nugget",
-            new Item(new FabricItemSettings().rarity(Rarity.RARE).group(ModItemGroup.Mio)));
-
-    public static final Item RAW_OSMIUM = registerItem("raw_osmium",
-            new Item(new FabricItemSettings().rarity(Rarity.RARE).group(ModItemGroup.Mio)));
-    public static final Item OSMIUM_INGOT = registerItem("osmium_ingot",
             new Item(new FabricItemSettings().rarity(Rarity.RARE).group(ModItemGroup.Mio)));
     public static final Item OSMIUM_NUGGET = registerItem("osmium_nugget",
             new Item(new FabricItemSettings().rarity(Rarity.RARE).group(ModItemGroup.Mio)));
 
-    public static final Item RAW_URANIUM = registerItem("raw_uranium",
-            new Item(new FabricItemSettings().rarity(Rarity.EPIC).group(ModItemGroup.Mio)));
-    public static final Item URANIUM_INGOT = registerItem("uranium_ingot",
-            new Item(new FabricItemSettings().rarity(Rarity.EPIC).group(ModItemGroup.Mio)));
+    //endregion
 
+    //region Special Items
 
     public static final Item ENHANCED_BONE_MEAL = registerItem("enhanced_bone_meal",
             new Item(new FabricItemSettings().rarity(Rarity.EPIC).group(ModItemGroup.Mio)));
 
     public static final Item DOWSING_ROD = registerItem("dowsing_rod",
             new DowsingRodItem(new FabricItemSettings().rarity(Rarity.EPIC).group(ModItemGroup.Mio).maxDamage(1250)));
+
+    //endregion
+
+    //region Fuels
 
     public static final Item MAHAGONY_COAL = registerItem("mahagony_coal",
             new Item(new FabricItemSettings().group(ModItemGroup.Mio)));
@@ -79,6 +91,7 @@ public class ModItems {
     public static final Item CHERRY_TREE_COAL = registerItem("cherry_tree_coal",
             new Item(new FabricItemSettings().group(ModItemGroup.Mio)));
 
+    //endregion
 
 
     private static Item registerItem(String name, Item item) {
