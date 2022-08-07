c2121f7f4cdcd2032945a46bd18333b0752a1994
Blue.PHS <bluelp@gmx.de>
2022-08-07 20:58:18 +0200

    Changed some stuff

--- src/main/java/net/bluephs/miocore/item/ModToolMaterials.java
+++ src/main/java/net/bluephs/miocore/item/ModToolMaterials.java
@@ -1,6 +1,5 @@
 package net.bluephs.miocore.item;
 
-import net.fabricmc.yarn.constants.MiningLevels;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.util.Lazy;
@@ -8,14 +7,15 @@ import net.minecraft.util.Lazy;
 import java.util.function.Supplier;
 
 public enum ModToolMaterials implements ToolMaterial {
+    PLATINUMTOOL(3, 2015, 12.0F, 2.0F, 10, () -> Ingredient.ofItems(ModNuggetItems.PLATINUM_NUGGET)),
+    OSMIUMTOOL(4, 4062, 15.0F, 2.0F, 15, () -> Ingredient.ofItems(ModNuggetItems.OSMIUM_NUGGET)),
     NICKEL(2, 164, 2.0F, 6.0F, 14, () -> Ingredient.ofItems(ModIngotItems.NICKEL_INGOT)),
     LEAD(2, 313, 4.0F, 9.0F, 15, () -> Ingredient.ofItems(ModIngotItems.LEAD_INGOT)),
     ALUMINIUM(2, 391, 6.0F, 12.0F, 14, () -> Ingredient.ofItems(ModIngotItems.ALUMINIUM_INGOT)),
     SILVER(3, 1951, 8.0F, 16.0F, 10, () -> Ingredient.ofItems(ModIngotItems.SILVER_INGOT)),
     PLATINUM(3, 2015, 12.0F, 20F, 10, () -> Ingredient.ofItems(ModIngotItems.PLATINUM_INGOT)),
-    OSMIUM(4, 4062, 15.0F, 9.0F, 15, () -> Ingredient.ofItems(ModIngotItems.OSMIUM_INGOT)),
-    PLATINUMTOOL(3, 2015, 12.0F, 2.0F, 10, () -> Ingredient.ofItems(ModNuggetItems.PLATINUM_NUGGET)),
-    OSMIUMTOOL(4, 4062, 15.0F, 2.0F, 15, () -> Ingredient.ofItems(ModNuggetItems.OSMIUM_NUGGET));
+    OSMIUM(4, 4062, 15.0F, 38.0F, 15, () -> Ingredient.ofItems(ModIngotItems.OSMIUM_INGOT));
+
 
     private final int miningLevel;
     private final int itemDurability;
